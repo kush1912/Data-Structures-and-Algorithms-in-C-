c61d7c534e827715d8dd2f53f2043796489bdd05
Ajay Kumar Kushwaha <vidyajaykushwaha@gmail.com>
2019-06-30 16:30:28 +0530

    Create max Axorb.cpp

---
+++ Bitmasking/max Axorb.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+
+int dec(int ct[])
+{
+    int p =1,ans= 0;
+	for(int i=0;i<32;i++)
+	{
+	    ans+=ct[i]*p;
+	    p = p<<1;      //p = p*2
+	}
+    return ans;
+}
+
+int axorb(int a,int b)
+{
+    int arr[32]={0};
+    int i = 0 ;
+    while(a>0|b>0)
+    {
+        //cout<<a<<b<<" "<<endl;
+        
+        if((a&1) && !(b&1))
+        {
+            arr[i] = 1;
+        }
+        if(!(a&1) && (b&1))
+        {
+            arr[i] = 1;
+        }
+        //cout<<arr[i]<<" "<<endl;
+        i++;
+        a = a>>1;
+        b = b>>1;
+    }
+    i--;
+    while(i>0)
+    {
+        if(arr[i]==1)
+        {
+            arr[i-1]=1;
+        }
+        i--;
+    }
+    return dec(arr);
+}
+
+int main() {
+	int a,b;
+	cin>>a>>b;
+	cout<< axorb(a,b);
+	
+	return 0;
+}
