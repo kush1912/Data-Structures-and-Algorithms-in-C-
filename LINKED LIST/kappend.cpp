d521c147a904ee6a1616eaeb45e27441cb8e42a5
Ajay Kumar Kushwaha <vidyajaykushwaha@gmail.com>
2019-07-13 11:58:50 +0530

    Create kappend.cpp

---
+++ LINKED LIST/kappend.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node *next;
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+//Insert at head
+void inserthead(node* &head,int d)
+{
+    node *n = new node(d);
+    n->next = head;
+    head = n;
+}
+
+//Insert at tail
+node* inserttail(node* &head,int d)
+{
+    if(head==NULL)
+    {
+        inserthead(head,d);
+        return head;
+    }
+    node*n = new node(d);
+    n->next = NULL;
+    node *temp = head;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next = n;
+    return head;
+}
+
+node* kappend(node*&head,int n)
+{
+    node* runner = head;
+    node* temp = head;
+    for(int i=0;i<n;i++)
+    {
+        runner = runner->next;
+    }
+    while(runner->next!=NULL)
+    {
+        temp= temp->next;
+        runner = runner->next;
+    }
+    runner->next = head;
+    head = temp->next;
+    temp->next=NULL;
+    return head;
+}
+
+void print(node *head)
+{
+    while(head!=NULL)
+    {
+        cout<<head->data<<" ";
+        head= head->next;
+    }
+    cout<<endl;
+}
+
+int main() {
+	int n,t,data;
+	cin>>n;
+	node *h1=NULL;
+	for(int i=0;i<n;i++)
+    {
+        cin>>data;
+    	h1 = inserttail(h1,data);
+    }
+    
+    cin>>t;
+    h1= kappend(h1,t);
+    print(h1);
+    
+	return 0;
+}
